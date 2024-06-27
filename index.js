0802ac2fd2319f585f7e1b65afa7478dbf3cb306
Wilson Cueva <wilsoncueva714@gmail.com>
2024-06-27 01:29:21 -0500

    chat model

--- index.js
+++ index.js
@@ -5,19 +5,26 @@ dotenv.config()
 
 const app = express()
 
-app.post('/generate', async (req, res) =>{
+app.use(express.json());
 
-  const replicate = new Replicate({
-    auth: process.env.REPLICATE_API_TOKEN,
-    userAgent: 'https://www.npmjs.com/package/create-replicate'
-  });
+const replicate = new Replicate({
+  auth: process.env.REPLICATE_API_TOKEN,
+  userAgent: 'https://www.npmjs.com/package/create-replicate'
+});
+
+app.post('/generate', async (req, res) =>{
+  const { prompt } = req.body;
+
+  if(!prompt){
+    return res.status(400).json({ error: 'El campo "prompt" es obligatorio en la solicitud.' });
+  }
 
   const model = 'stability-ai/sdxl:7762fd07cf82c948538e41f63f77d685e02b063e37e496e96eefd46c929f9bdc'
   
   const input = {
     width: 768,
     height: 768,
-    prompt: 'An astronaut driving a red car, cinematic, dramatic',
+    prompt,
     refine: 'expert_ensemble_refiner',
     scheduler: 'K_EULER',
     lora_scale: 0.6,
@@ -36,6 +43,33 @@ app.post('/generate', async (req, res) =>{
   
   res.send('Generando')
 });
+
+app.post('/chat', async (req, res) => {
+
+  const { prompt } = req.body;
+
+  const input = {
+    top_k: 0,
+    top_p: 0.9,
+    prompt,
+    max_tokens: 512,
+    min_tokens: 0,
+    temperature: 0.6,
+    system_prompt: "You are a helpful assistant",
+    length_penalty: 1,
+    stop_sequences: "<|end_of_text|>,<|eot_id|>",
+    prompt_template: "<|begin_of_text|><|start_header_id|>system<|end_header_id|>\n\nYou are a helpful assistant<|eot_id|><|start_header_id|>user<|end_header_id|>\n\n{prompt}<|eot_id|><|start_header_id|>assistant<|end_header_id|>\n\n",
+    presence_penalty: 1.15,
+    log_performance_metrics: false
+  };
+  
+  for await (const event of replicate.stream("meta/meta-llama-3-70b-instruct", { input })) {
+    process.stdout.write(event.toString());
+  };
+  res.end()
+
+
+})
 
 
 app.listen(3000, () => {
